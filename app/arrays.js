6069e06437379f8c897e51c0c3e342ffb941909b
Dion <Dion.Fulwood@gmail.com>
2016-11-15 15:20:29 -0800

    Completes indexOf, sum, remove, removeWithoutCopy and append

--- app/arrays.js
+++ app/arrays.js
@@ -1,25 +1,48 @@
+
+
 exports = typeof window === 'undefined' ? global : window;
+
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    var index = -1
+    if(arr.includes(item)) {
+      for(var i = 0; i < arr.length; i++) {
+        if(arr[i] === item) {
+          index = i;
+          break;
+        }
+      }
+    }
+   
+    return index;
   },
 
   sum: function(arr) {
+    return arr.reduce((a, b) => {
+      return a + b;
+    });
 
   },
 
   remove: function(arr, item) {
-
+    return arr.filter((element)=> {
+      return element !== item;
+    });
   },
 
   removeWithoutCopy: function(arr, item) {
+    while(arr.includes(item)) {
+      arr.splice(arr.indexOf(item), 1);
 
+    }
+    return arr;
   },
 
   append: function(arr, item) {
-
-  },
+    arr.push(item);
+    return arr;
+  },  
 
   truncate: function(arr) {
 
