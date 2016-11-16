a8386eab44d63083d9bf3715b2462e8554f3dd7d
Dion <Dion.Fulwood@gmail.com>
2016-11-15 17:47:15 -0800

    Completes argAsArray, speak, functionFunction, and makeClosures

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,30 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+   return fn.apply(null, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return function(arg) {
+      return str + ', ' + arg;
+    };
   },
 
   makeClosures: function(arr, fn) {
+    let cbList = [];
+    const innerFunction = (val) => {
+      return () => { return fn(val); };
+    };
 
+    for (let i = 0, len = arr.length; i < len; i++) {
+      ret.push(innerFunction(arr[i]));
+    }
+
+    return cbList;
   },
 
   partial: function(fn, str1, str2) {
