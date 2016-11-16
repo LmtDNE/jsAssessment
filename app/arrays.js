58e22ccbc17f78e7f47eda3f506dae137e2d9a18
Dion <Dion.Fulwood@gmail.com>
2016-11-15 16:12:53 -0800

    Completes append, truncate, prepend, curtail, concat, insert, count and duplicates

--- app/arrays.js
+++ app/arrays.js
@@ -45,31 +45,56 @@ exports.arraysAnswers = {
   },  
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return  arr;
   },
 
   concat: function(arr1, arr2) {
-
+     return [...arr1,...arr2];
   },
 
   insert: function(arr, item, index) {
-
+    // console.log('This is arr', arr.length);
+    // console.log('This is item', item);
+    // console.log('This is index', index);
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    var count = arr.filter((element) => {
+      return element === item;
+    });
+    return count.length;
   },
 
   duplicates: function(arr) {
+   console.log("This is arr", arr);
+   var result = [];
+   var hash = {}
 
+   arr.forEach(element => {
+    if(!hash.hasOwnProperty(element)) {
+      console.log('This is the element', element)
+      hash[element] = element
+    }else{
+      if(!result.includes(element)) {
+        result.push(element)
+      }
+    }
+   });
+   console.log('This is result', result);
+   return result;
   },
 
   square: function(arr) {
